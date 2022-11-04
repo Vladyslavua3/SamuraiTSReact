fce8961ca7014428861962148b28edd45c247b73
Vladyslavua <vladgershman@gmail.com>
2022-11-04 20:06:05 +0100

    update Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,6 +9,9 @@ export const Header = (props:HeaderPropsType) => {
     return (
         <div className="App">
             <h1>{props.title}</h1>
+            <a href='#'>Home</a>
+            <a href='#'>News Feed</a>
+            <a href='#'>Messages</a>
         </div>
     )
 }
