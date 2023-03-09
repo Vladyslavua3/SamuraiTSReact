5a10e6fab848f1dfb612c9f0e93471f965adf2a9
Vladyslavua <vladgershman@gmail.com>
2023-03-09 16:06:19 +0100

    init:Redux Form

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import './App.css';
-import {Header} from "./components/Header/Header";
 import {Nav} from "./components/Nav/Nav";
 import { Route} from "react-router-dom";
 import {News} from "./components/News/News";
--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,9 +1,44 @@
 import React from "react";
+import { Field, InjectedFormProps, reduxForm } from "redux-form";
 
-export const Login = () => {
+
+interface FormData{
+  login:string
+  password:string
+  rememberMe:boolean
+}
+
+export const LoginForm:React.FC<InjectedFormProps<FormData>> = (props) => {
   return (
-    <div>
-      <h1>LOGIN</h1>
-    </div>
+    <form onSubmit={props.handleSubmit}>
+      <div>
+        <Field placeholder={'Login'} name={'login'} component={'input'}/>
+      </div>
+      <div>
+        <Field placeholder={'Password'} name={'password'} component={'input'}/>
+      </div>
+      <div>
+        <Field type={'checkbox'} name={'rememberMe'} component={'input'}/> remember me
+      </div>
+      <div>
+        <button>Login</button>
+      </div>
+    </form>
   );
 };
+
+const LoginReduxForm = reduxForm<FormData>({form:'login'})(LoginForm)
+
+
+export const Login = () => {
+  const onSubmit = (formData:FormData) => {
+    console.log(formData);
+  }
+
+  return(
+    <div>
+      <h1>Login</h1>
+      <LoginReduxForm onSubmit={onSubmit}/>
+    </div>
+  )
+}
